93f2fb13be80665e0d02bb670173f3575a731860
Larry Hack <larry.hack@blinds.com>
2012-07-29 02:54:26 -0500

    worked on ui for adding tank info

--- app/Phorcys.UI.Web/Models/GearModel.cs
+++ app/Phorcys.UI.Web/Models/GearModel.cs
@@ -25,18 +25,17 @@ namespace Phorcys.UI.Web.Models {
     public string Notes { get; set; }
 
     //additional tank data
-    /*
-    [Range(0,5000)]
+    //[Range(0,5000)]
     public int TankVolume { get; set; }
 
-    [Range(0,5000)]
+    //[Range(0,5000)]
     public int WorkingPressure { get; set; }
 
-    [Range(0,12)]
+    //[Range(0,12)]
     public int ManufacturedMonth { get; set; }
 
-    [Range(0,99)]
+    //[Range(0,99)]
     public int ManufacturedYear { get; set; }
-     */
+    
   }
 }

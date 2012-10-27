1e67008c37e280a8f48543b9d704b188e8c20687
Larry Hack <larry.hack@blinds.com>
2012-10-26 20:46:44 -0500

    Add MOD

---
+++ app/Phorcys.UI.Web/Controllers/ModController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Phorcys.UI.Web.Controllers
+{
+    public class ModController : Controller
+    {
+        // GET: /Mod/
+        public ActionResult Index()
+        {
+            return View();
+        }
+ }
+}

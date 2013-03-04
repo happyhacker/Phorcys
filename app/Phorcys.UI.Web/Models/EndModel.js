9cacfb1177ce66cce94ee469ec67c4ff5a9bdb0e
Larry Hack <larry.hack@blinds.com>
2013-03-03 18:26:14 -0600

    Moved Knockout model to seperate js file.

---
+++ app/Phorcys.UI.Web/Models/EndModel.js
@@ -0,0 +1,15 @@
+﻿
+  var viewModel = {
+    O2: ko.observable(21),
+    He: ko.observable(0),
+    Depth: ko.observable(),
+    END: ko.observable()
+  };
+
+  function Calc() {
+    if (parseInt(viewModel.He()) + parseInt(viewModel.O2()) > 100) {
+      alert("O2 + Helium can't exceed 100%");
+    } else {
+      viewModel.END(CalcEND(viewModel.He(), viewModel.Depth(), true));
+    }
+  }

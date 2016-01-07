eb029815e294a65c20b478bba93712456711c6ba
HappyHacker <Larry.hack@blinds.com>
2016-01-06 21:49:12 -0600

    Many changes related to changes in the database.

--- db/schema/alter2014.4.23.sql
+++ db/schema/alter2014.4.23.sql
@@ -1,125 +1,65 @@
 /* ---------------------------------------------------------------------- */
-/* Script generated with: DeZign for Databases V7.3.6                     */
+/* Script generated with: DeZign for Databases V9.1.0                     */
 /* Target DBMS:           MS SQL Server 2012                              */
 /* Project file:          SCUBA.dez                                       */
 /* Project name:                                                          */
 /* Author:                                                                */
 /* Script type:           Alter database script                           */
-/* Created on:            2014-04-23 18:25                                */
+/* Created on:            2016-01-06 19:20                                */
 /* ---------------------------------------------------------------------- */
-
-
-/* ---------------------------------------------------------------------- */
-/* Drop views                                                             */
-/* ---------------------------------------------------------------------- */
-
-DROP VIEW [dbo].[vwCertifications]
-GO
 
 
 /* ---------------------------------------------------------------------- */
 /* Drop foreign key constraints                                           */
 /* ---------------------------------------------------------------------- */
 
-ALTER TABLE [dbo].[DiverCertifications] DROP CONSTRAINT [Divers_DiverCertifications]
 GO
 
 
-ALTER TABLE [dbo].[DiverCertifications] DROP CONSTRAINT [Certifications_DiverCertifications]
+ALTER TABLE [DiveTeams] DROP CONSTRAINT [Divers_DiveTeam]
 GO
 
 
-ALTER TABLE [dbo].[DiverCertifications] DROP CONSTRAINT [Instructors_DiverCertifications]
+ALTER TABLE [DiveTeams] DROP CONSTRAINT [Dives_DiveTeam]
+GO
+
+
+ALTER TABLE [Dives] DROP CONSTRAINT [Dives_DiveDetails]
+GO
+
+
+ALTER TABLE [Dives] DROP CONSTRAINT [Roles_DiveDetails]
+GO
+
+
+ALTER TABLE [Dives] DROP CONSTRAINT [Users_Dives]
+GO
+
+
+ALTER TABLE [DiveUrls] DROP CONSTRAINT [DiveDetails_ContentLinks]
 GO
 
 
 /* ---------------------------------------------------------------------- */
-/* Drop and recreate table "dbo.DiverCertifications"                      */
+/* Alter table "DiveTeams"                                                */
 /* ---------------------------------------------------------------------- */
 
-ALTER TABLE [dbo].[DiverCertifications] DROP CONSTRAINT [DEF_DiverCertifications_Created]
 GO
 
 
-ALTER TABLE [dbo].[DiverCertifications] DROP CONSTRAINT [DEF_DiverCertifications_LastModified]
+ALTER TABLE [DiveTeams] ADD
+    [RoleId] INTEGER
 GO
 
 
-ALTER TABLE [dbo].[DiverCertifications] DROP CONSTRAINT [PK_DiverCertifications]
+/* ---------------------------------------------------------------------- */
+/* Alter table "Dives"                                                    */
+/* ---------------------------------------------------------------------- */
+
 GO
 
 
-CREATE TABLE [dbo].[DiverCertifications_TMP] (
-    [DiverCertificationId] INTEGER IDENTITY(1,1) NOT NULL,
-    [DiverId] INTEGER NOT NULL,
-    [CertificationId] INTEGER NOT NULL,
-    [Certified] DATE,
-    [CertificationNum] VARCHAR(20) NOT NULL,
-    [Notes] VARCHAR(max),
-    [Created] DATETIME CONSTRAINT [DEF_DiverCertifications_Created] DEFAULT getdate() NOT NULL,
-    [LastModified] DATETIME CONSTRAINT [DEF_DiverCertifications_LastModified] DEFAULT getdate() NOT NULL,
-    [InstructorId] INTEGER)
-GO
-
-
-INSERT INTO [dbo].[DiverCertifications_TMP]
-    ([DiverId],[CertificationId],[Certified],[CertificationNum],[Notes],[Created],[LastModified],[InstructorId])
-SELECT
-    [DiverId],[CertificationId],[Certified],[CertificationNum],[Notes],[Created],[LastModified],[InstructorId]
-FROM [dbo].[DiverCertifications]
-GO
-
-
-DROP TABLE [dbo].[DiverCertifications]
-GO
-
-
-EXEC sp_rename '[dbo].[DiverCertifications_TMP]', 'DiverCertifications', 'OBJECT'
-GO
-
-
-ALTER TABLE [dbo].[DiverCertifications] ADD CONSTRAINT [PK_DiverCertifications] 
-    PRIMARY KEY CLUSTERED ([DiverCertificationId])
-GO
-
-
-EXECUTE sp_addextendedproperty N'MS_Description', N'N', 'SCHEMA', N'dbo', 'TABLE', N'DiverCertifications', NULL, NULL
-GO
-
-
-EXECUTE sp_addextendedproperty N'MS_Description', N'N', 'SCHEMA', N'dbo', 'TABLE', N'DiverCertifications', 'COLUMN', N'DiverCertificationId'
-GO
-
-
-EXECUTE sp_addextendedproperty N'MS_Description', N'N', 'SCHEMA', N'dbo', 'TABLE', N'DiverCertifications', 'COLUMN', N'DiverId'
-GO
-
-
-EXECUTE sp_addextendedproperty N'MS_Description', N'N', 'SCHEMA', N'dbo', 'TABLE', N'DiverCertifications', 'COLUMN', N'CertificationId'
-GO
-
-
-EXECUTE sp_addextendedproperty N'MS_Description', N'N', 'SCHEMA', N'dbo', 'TABLE', N'DiverCertifications', 'COLUMN', N'Certified'
-GO
-
-
-EXECUTE sp_addextendedproperty N'MS_Description', N'N', 'SCHEMA', N'dbo', 'TABLE', N'DiverCertifications', 'COLUMN', N'CertificationNum'
-GO
-
-
-EXECUTE sp_addextendedproperty N'MS_Description', N'N', 'SCHEMA', N'dbo', 'TABLE', N'DiverCertifications', 'COLUMN', N'Notes'
-GO
-
-
-EXECUTE sp_addextendedproperty N'MS_Description', N'N', 'SCHEMA', N'dbo', 'TABLE', N'DiverCertifications', 'COLUMN', N'Created'
-GO
-
-
-EXECUTE sp_addextendedproperty N'MS_Description', N'N', 'SCHEMA', N'dbo', 'TABLE', N'DiverCertifications', 'COLUMN', N'LastModified'
-GO
-
-
-EXECUTE sp_addextendedproperty N'MS_Description', N'N', 'SCHEMA', N'dbo', 'TABLE', N'DiverCertifications', 'COLUMN', N'InstructorId'
+ALTER TABLE [Dives] DROP COLUMN [RoleId]
 GO
 
 
@@ -127,38 +67,35 @@ GO
 /* Add foreign key constraints                                            */
 /* ---------------------------------------------------------------------- */
 
-ALTER TABLE [dbo].[DiverCertifications] ADD CONSTRAINT [Divers_DiverCertifications] 
-    FOREIGN KEY ([DiverId]) REFERENCES [dbo].[Divers] ([DiverId])
 GO
 
 
-ALTER TABLE [dbo].[DiverCertifications] ADD CONSTRAINT [Certifications_DiverCertifications] 
-    FOREIGN KEY ([CertificationId]) REFERENCES [dbo].[Certifications] ([CertificationId])
+ALTER TABLE [DiveTeams] ADD CONSTRAINT [Dives_DiveTeam] 
+    FOREIGN KEY ([DivePlanId]) REFERENCES [DivePlans] ([DivePlanId])
 GO
 
 
-ALTER TABLE [dbo].[DiverCertifications] ADD CONSTRAINT [Instructors_DiverCertifications] 
-    FOREIGN KEY ([InstructorId]) REFERENCES [dbo].[Instructors] ([InstructorId])
+ALTER TABLE [DiveTeams] ADD CONSTRAINT [Divers_DiveTeam] 
+    FOREIGN KEY ([DiverId]) REFERENCES [Divers] ([DiverId])
 GO
 
 
-/* ---------------------------------------------------------------------- */
-/* Repair/add views                                                       */
-/* ---------------------------------------------------------------------- */
+ALTER TABLE [DiveTeams] ADD CONSTRAINT [Roles_DiveTeams] 
+    FOREIGN KEY ([RoleId]) REFERENCES [Roles] ([RoleId])
+GO
 
-CREATE VIEW [dbo].[vwCertifications] AS (
-SELECT certs.CertificationId, certs.Title, divers.DiverId, agency.Company AS Agency,
-dc.Certified, diverContact.FirstName AS DiverFirstName, diverContact.LastName AS DiverLastName,
-instructor.FirstName AS InstructorFirstName, instructor.LastName AS InstructorLastName
-FROM  dbo.Certifications AS certs INNER JOIN
-         dbo.DiverCertifications AS dc ON dc.CertificationId = certs.CertificationId INNER JOIN
-         dbo.Users AS u ON u.UserId = dc.DiverId INNER JOIN
-         dbo.Divers AS divers ON divers.DiverId = dc.DiverId INNER JOIN
-         dbo.Contacts AS diverContact ON diverContact.ContactId = divers.ContactId LEFT OUTER JOIN
-		 dbo.Instructors ON Instructors.InstructorId = dc.InstructorId LEFT OUTER JOIN
-         dbo.Contacts AS instructor ON instructor.ContactId = Instructors.ContactId INNER JOIN
-         dbo.DiveAgencies AS da ON da.DiveAgencyId = certs.DiveAgencyId INNER JOIN
-         dbo.Contacts AS agency ON agency.ContactId = da.ContactId
-		 )
+
+ALTER TABLE [Dives] ADD CONSTRAINT [Dives_DiveDetails] 
+    FOREIGN KEY ([DivePlanId]) REFERENCES [DivePlans] ([DivePlanId])
+GO
+
+
+ALTER TABLE [Dives] ADD CONSTRAINT [Users_Dives] 
+    FOREIGN KEY ([UserId]) REFERENCES [Users] ([UserId])
+GO
+
+
+ALTER TABLE [DiveUrls] ADD CONSTRAINT [DiveDetails_ContentLinks] 
+    FOREIGN KEY ([DiveId]) REFERENCES [Dives] ([DiveId])
 GO
 

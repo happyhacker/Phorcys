6ac912b0c007fc39aa7fa47b6f5861c7c4f171c5
Larry Hack <Larry@Hacksoft.net>
2023-08-11 14:00:57 -0500

    Added AddTankToDivePlan and the domain objects which should have been added a long time ago

---
+++ ConsoleApps/AddTankToDivePlan/Domain/AttributeAssociation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddDivePlan.Domain;
+
+public partial class AttributeAssociation
+{
+    public int AttributeId { get; set; }
+
+    public int TableRowId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string? Notes { get; set; }
+
+    public virtual Attribute Attribute { get; set; } = null!;
+}
---
+++ ConsoleApps/AddTankToDivePlan/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using AddDivePlan.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+
+class Program
+{
+
+    private static ScubaContext _context = new ScubaContext();
+
+    static async Task Main(string[] args)
+    {
+        var tanksOnDive = new TanksOnDive();
+        Console.Write("DivePlanId ");
+        tanksOnDive.DivePlanId = Int32.Parse(Console.ReadLine());
+        Console.Write("GearId ");
+        tanksOnDive.GearId = Int32.Parse(Console.ReadLine());
+        Console.Write("Content Title ");
+        tanksOnDive.GasContentTitle = Console.ReadLine();
+        Console.Write("Starting Pressure ");
+        tanksOnDive.StartingPressure = Int32.Parse(Console.ReadLine());
+        Console.Write("Ending Pressure ");
+        tanksOnDive.EndingPressure = Int32.Parse(Console.ReadLine());
+        AddTank(tanksOnDive);
+
+        Console.WriteLine("Press any key to continue");
+        Console.ReadKey();
+
+    }
+
+    private static void AddTank(TanksOnDive tanksOnDive) { 
+   
+        _context.TanksOnDives.Add(tanksOnDive);
+        _context.SaveChanges();
+    }
+
+}

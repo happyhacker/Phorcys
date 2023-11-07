c9707181a60182dc31f21a9d1b8462934cd2080b
Larry Hack <Larry@Hacksoft.net>
2023-11-06 19:32:01 -0600

    Saving with 1 error. Getting random errors that go away when I restart VS

--- app/Phorcys.UI.Web/Controllers/DivePlansController.cs
+++ app/Phorcys.UI.Web/Controllers/DivePlansController.cs
@@ -12,118 +12,151 @@ using NHibernate.Validator.Engine;
 using System.Text;
 using SharpArch.Web.CommonValidator;
 using SharpArch.Core;
+using MvcContrib.Sorting;
+using System.Linq;
 
-namespace Phorcys.Web.Controllers {
-  [HandleError]
-  public class DivePlansController : Controller {
-    private UserServices userServices = new UserServices();
-    private User user;
-    private readonly IRepository<DivePlan> divePlanRepository;
-    public DivePlansController(IRepository<DivePlan> divePlanRepository) {
-      Check.Require(divePlanRepository != null, "divePlanRepository may not be null");
-      this.divePlanRepository = divePlanRepository;
-    }
+namespace Phorcys.Web.Controllers
+{
+    [HandleError]
+    public class DivePlansController : Controller
+    {
+        private UserServices userServices = new UserServices();
+        private readonly IDiveSiteServices diveSiteServices = new DiveSiteServices();
+        private User user;
+        private readonly IRepository<DivePlan> divePlanRepository;
+        private DiveSitesModel viewModel = new DiveSitesModel();
 
-    [Authorize]
-    [Transaction]
-    public ActionResult Index() {
-      IList<DivePlan> divePlans = divePlanRepository.GetAll();
-      return View(divePlans);
-    }
+        public DivePlansController(IRepository<DiveSite> diveSiteRepository, IRepository<DivePlan> divePlanRepository, IRepository<User> userRepository)
+        {
+            Check.Require(divePlanRepository != null, "divePlanRepository may not be null");
+            Check.Require(diveSiteRepository != null, "diveSiteRepository may not be null");
+            Check.Require(userRepository != null, "userRepository may not be null");
 
-    [Authorize]
-    [AcceptVerbs(HttpVerbs.Get)]
-    public ActionResult Create() {
-      DivePlanModel model = new DivePlanModel();
-      return View(model);
-    }
-
-    [Authorize]
-    [ValidateAntiForgeryToken]
-    [Transaction]
-    [AcceptVerbs(HttpVerbs.Post)]
-    public ActionResult Create(DivePlanModel model) {
-      DivePlan divePlan  = new DivePlan();
-      user = userServices.FindUser(this.User.Identity.Name);
-      divePlan.User = user;
-      divePlan.Title = model.Title;
-      divePlan.Notes = model.Notes;
-      divePlan.MaxDepth = model.MaxDepth;
-      divePlan.ScheduledTime = model.ScheduledTime;
-      divePlan.Created = DateTime.Now;
-      divePlan.LastModified = DateTime.Now;
-      divePlanRepository.SaveOrUpdate(divePlan);
-      TempData[ControllerEnums.GlobalViewDataProperty.PageMessage.ToString()] =
-         "The Dive Plan was successfully created.";
-      return RedirectToAction("Index");
-
-      //return View(model);
-    }
-
-    [Authorize]
-    [AcceptVerbs(HttpVerbs.Get)]
-    [Transaction]
-    public ActionResult Edit(int id) {
-      DivePlanModel model = new DivePlanModel();
-      DivePlan divePlan = divePlanRepository.Get(id);
-      model.Id = divePlan.Id;
-      model.Title = divePlan.Title;
-      model.Notes = divePlan.Notes;
-      return View(model);
-    }
-
-    [Authorize]
-    [ValidateAntiForgeryToken]
-    [Transaction]
-    [AcceptVerbs(HttpVerbs.Post)]
-    public ActionResult Edit(DivePlanModel divePlanModel) {
-      DivePlan divePlanToUpdate = divePlanRepository.Get(divePlanModel.Id);
-      TransferFormValuesTo(divePlanToUpdate, divePlanModel);
-      divePlanToUpdate.LastModified = DateTime.Now;
-
-      if (ModelState.IsValid) {
-        divePlanRepository.SaveOrUpdate(divePlanToUpdate);
-
-        TempData[ControllerEnums.GlobalViewDataProperty.PageMessage.ToString()] =
-  "The Dive Plan was successfully updated.";
-        return RedirectToAction("Index");
-      } else {
-        divePlanRepository.DbContext.RollbackTransaction();
-
-        return View(divePlanModel);
-      }
-    }
-
-    private void TransferFormValuesTo(DivePlan divePlanToUpdate, DivePlanModel divePlanFromForm) {
-      divePlanToUpdate.Title = divePlanFromForm.Title;
-      divePlanToUpdate.Notes = divePlanFromForm.Notes;
-    }
-
-    [Authorize]
-    [ValidateAntiForgeryToken]
-    [Transaction]
-    [AcceptVerbs(HttpVerbs.Post)]
-    public ActionResult Delete(int id) {
-      string resultMessage = "The Dive Plan was successfully deleted.";
-      DivePlan divePlanToDelete = divePlanRepository.Get(id);
-
-      if (divePlanToDelete != null) {
-        divePlanRepository.Delete(divePlanToDelete);
-
-        try {
-          divePlanRepository.DbContext.CommitChanges();
-        } catch(Exception e) {
-          resultMessage = "A problem was encountered preventing the Dive Plan from being deleted. " +
-                          "Another item likely depends on this Dive Plan.";
-          divePlanRepository.DbContext.RollbackTransaction();
+            this.divePlanRepository = divePlanRepository;
         }
-      } else {
-        resultMessage = "The Dive Plan could not be found for deletion. It may already have been deleted.";
-      }
 
-      TempData[ControllerEnums.GlobalViewDataProperty.PageMessage.ToString()] = resultMessage;
-      return RedirectToAction("Index");
+        [Authorize]
+        [Transaction]
+        public ActionResult Index()
+        {
+            user = userServices.FindUser(this.User.Identity.Name);
+            IList<DivePlan> divePlans = divePlanRepository.GetAll();
+
+            return View(divePlans);
+        }
+
+        [Authorize]
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult Create()
+        {
+            DivePlanModel model = new DivePlanModel();
+            user = userServices.FindUser(this.User.Identity.Name);
+            IList<DiveSite> diveSites = diveSiteServices.GetAllForUser(user.Id);
+            diveSites = diveSites.OrderBy(d => d.Title).ToList();
+            model.DiveSiteList = diveSites;
+            return View(model);
+        }
+
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        [Transaction]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create(DivePlanModel model)
+        {
+            DivePlan divePlan = new DivePlan();
+            user = userServices.FindUser(this.User.Identity.Name);
+            divePlan.User = user;
+            divePlan.Title = model.Title;
+            divePlan.Notes = model.Notes;
+            divePlan.MaxDepth = model.MaxDepth;
+            divePlan.ScheduledTime = model.ScheduledTime;
+            divePlan.Created = DateTime.Now;
+            divePlan.LastModified = DateTime.Now;
+            divePlanRepository.SaveOrUpdate(divePlan);
+            TempData[ControllerEnums.GlobalViewDataProperty.PageMessage.ToString()] =
+               "The Dive Plan was successfully created.";
+            return RedirectToAction("Index");
+
+            //return View(model);
+        }
+
+        [Authorize]
+        [AcceptVerbs(HttpVerbs.Get)]
+        [Transaction]
+        public ActionResult Edit(int id)
+        {
+            DivePlanModel model = new DivePlanModel();
+            DivePlan divePlan = divePlanRepository.Get(id);
+            model.Id = divePlan.Id;
+            model.Title = divePlan.Title;
+            model.Notes = divePlan.Notes;
+            return View(model);
+        }
+
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        [Transaction]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Edit(DivePlanModel divePlanModel)
+        {
+            DivePlan divePlanToUpdate = divePlanRepository.Get(divePlanModel.Id);
+            TransferFormValuesTo(divePlanToUpdate, divePlanModel);
+            divePlanToUpdate.LastModified = DateTime.Now;
+
+            if (ModelState.IsValid)
+            {
+                divePlanRepository.SaveOrUpdate(divePlanToUpdate);
+
+                TempData[ControllerEnums.GlobalViewDataProperty.PageMessage.ToString()] =
+          "The Dive Plan was successfully updated.";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                divePlanRepository.DbContext.RollbackTransaction();
+
+                return View(divePlanModel);
+            }
+        }
+
+        private void TransferFormValuesTo(DivePlan divePlanToUpdate, DivePlanModel divePlanFromForm)
+        {
+            divePlanToUpdate.Title = divePlanFromForm.Title;
+            divePlanToUpdate.Notes = divePlanFromForm.Notes;
+        }
+
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        [Transaction]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Delete(int id)
+        {
+            string resultMessage = "The Dive Plan was successfully deleted.";
+            DivePlan divePlanToDelete = divePlanRepository.Get(id);
+
+            if (divePlanToDelete != null)
+            {
+                divePlanRepository.Delete(divePlanToDelete);
+
+                try
+                {
+                    divePlanRepository.DbContext.CommitChanges();
+                }
+                catch (Exception e)
+                {
+                    resultMessage = "A problem was encountered preventing the Dive Plan from being deleted. " +
+                                    "Another item likely depends on this Dive Plan.";
+                    divePlanRepository.DbContext.RollbackTransaction();
+                }
+            }
+            else
+            {
+                resultMessage = "The Dive Plan could not be found for deletion. It may already have been deleted.";
+            }
+
+            TempData[ControllerEnums.GlobalViewDataProperty.PageMessage.ToString()] = resultMessage;
+            return RedirectToAction("Index");
+        }
+
     }
-
-  }
 }
--- app/Phorcys.UI.Web/Models/DivePlanModel.cs
+++ app/Phorcys.UI.Web/Models/DivePlanModel.cs
@@ -1,20 +1,24 @@
-﻿using System;
+﻿using Phorcys.Core;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Phorcys.UI.Web.Models {
-  public class DivePlanModel {
+namespace Phorcys.UI.Web.Models
+{
+    public class DivePlanModel
+    {
 
-    public int Id { get; set; }
-    [Required]
-    public string Title { get; set; }
-    public DateTime ScheduledTime { get; set; }
-    public int MaxDepth { get; set; }
-    public string Notes { get; set; }
-    public DateTime Created { get; set; }
-    public DateTime LastModified { get; set; } 
-  }
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public IList<SelectListItem> DiveSiteList { get; set; }
+        public DateTime ScheduledTime { get; set; }
+        public int MaxDepth { get; set; }
+        public string Notes { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastModified { get; set; }
+    }
 }

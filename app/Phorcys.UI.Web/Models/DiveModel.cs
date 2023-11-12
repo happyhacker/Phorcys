f067ff1fad9f00d176f8d10ee3e9b83d81c8d1da
Larry Hack <Larry@Hacksoft.net>
2023-11-12 00:00:26 -0600

    Saving days work, added UI stuff, not controller stuff yet

---
+++ app/Phorcys.UI.Web/Models/DiveModel.cs
@@ -0,0 +1,30 @@
+﻿using Phorcys.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Phorcys.UI.Web.Models
+{
+    public class DiveModel
+    {
+
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public IList<SelectListItem> DivePlanList { get; set; }
+        public int DivePlanId { get; set; }
+        public int DiveNumber { get; set; }        
+        public DateTime DescentTime { get; set; }
+        public int Minutes { get; set; }
+        public int AvgDepth { get; set; }
+        public int MaxDepth { get; set; }
+        public int Temperature { get; set; }
+        public int AdditionalWeight { get; set; }
+        public string Notes { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastModified { get; set; }
+    }
+}

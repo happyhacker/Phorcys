7f6b27e91fc8733576299f436ad88eb2d7cc2a98
Larry Hack <Larry@Hacksoft.net>
2023-12-05 16:14:37 -0600

    adding alter.sql

---
+++ db/alter.sql
@@ -0,0 +1,10 @@
+/* ---------------------------------------------------------------------- */
+/* Script generated with: DeZign for Databases 12.3.1                     */
+/* Target DBMS:           MS SQL Server 2017                              */
+/* Project file:          SCUBA.dez                                       */
+/* Project name:                                                          */
+/* Author:                                                                */
+/* Script type:           Alter database script                           */
+/* Created on:            2023-12-05 15:46                                */
+/* ---------------------------------------------------------------------- */
+
